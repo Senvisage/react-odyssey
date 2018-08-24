49abc85685b72b775caf55996cac3435ef0d1f3f
Senvisage <senvisage.spam@gmail.com>
2018-08-24 14:42:36 +0200

    Create combine and use reducers

--- front/front-react/src/index.js
+++ front/front-react/src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import allReducers from "./reducers/index.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(allReducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
